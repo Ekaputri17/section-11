afc65457265b62514051842d8f5c65fb73ef757c
Ekaputri17 <ekaputriw1@gmail.com>
2023-08-19 20:32:34 +0700

    menambahkan folder SQL_Join

--- SQL_Join/tugas_left join.sql
+++ SQL_Join/tugas_left join.sql
@@ -1,4 +1,5 @@
 
 SELECT users.name, users.id, products.user_id, products.category_id, categories.user_id FROM users 
 LEFT JOIN products ON users.id = products.id
-LEFT JOIN categories ON products.category_id  = categories.id `tugas_left join`
+LEFT JOIN categories ON products.category_id  = categories.id 
+ORDER BY users.id
